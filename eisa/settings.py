4f2903f620bafcd5398aabb6426aa8999a3841fe
easanahardani <92developer92@gmail.com>
2022-02-26 04:21:09 +0330

    start security

--- eisa/settings.py
+++ eisa/settings.py
@@ -201,6 +201,10 @@ STATICFILES_DIRS = [(BASE_DIR / 'static')]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 ###############################################################################
 #                                Messages                                     #
 ###############################################################################
